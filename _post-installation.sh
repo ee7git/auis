a9e348461db4e945100918aebd47df97ed5a97b0
Warren <warren.eaton.sciaccaluga@gmail.com>
2020-05-12 22:58:39 -0400

    Update _post-installation.sh

--- _post-installation.sh
+++ _post-installation.sh
@@ -36,7 +36,7 @@ cat <<EOF > /mnt/etc/X11/xorg.conf.d/00-keyboard.conf
 Section "InputClass"
     Identifier "keyboard languages"
     MatchIsKeyboard "on"
-    Option "XkbLayout" "latam,us,ru"
+    Option "XkbLayout" "latam,et,ru"
     Option "XkbModel" "pc104"
     Option "XkbVariant" ",,"
     Option "XkbOptions" "grp:alt_shift_toggle"
